18eddabdee768511879f70312b7c068d677f2396
Iryna Volosozharova <iradevis@gmail.com>
2021-05-30 01:41:32 +0300

    Added fetch fn, render markup and base styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import './sass/main.scss';
-import API from './js/apiService';
+import cardTmpl from './templates/cardTmpl.hbs';
+import PicApiService from './js/apiService';
 import getRefs from './js/get-refs';
-const refs = getRefs();
 
+const refs = getRefs();
+const picApiService = new PicApiService();
+
+refs.searchForm.addEventListener('submit', onSearch)
+refs.loadBtn.addEventListener('click', onLoadMore)
+
+function onSearch(e) {
+    e.preventDefault()
+    picApiService.query = e.currentTarget.elements.query.value;
+    picApiService.resetPage();
+    picApiService.fetchPic().then(renderPicMarkup);
+}
+
+function onLoadMore() {
+    picApiService.fetchPic().then(renderPicMarkup);
+}
+
+function renderPicMarkup(hits) {
+   refs.gallery.insertAdjacentHTML('beforeend', cardTmpl(hits))
+}
