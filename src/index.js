4b2d085fb2867299b45693ec01d808cf418992c1
Iryna Volosozharova <iradevis@gmail.com>
2021-05-29 22:33:15 +0300

    Added start files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './sass/main.scss';
+import API from './js/apiService';
+import getRefs from './js/get-refs';
+const refs = getRefs();
+
---
+++ src/js/apiService.js
@@ -0,0 +1,6 @@
+const KEY = '21850278-bca2574dc72e96cdde1e95c4f';
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&';
+const per_page = 12;
+
+
+
